858da471c0af2b2544e5737215a3ef3ab5d96728
Alsu <shakilsu@yandex.ru>
2022-11-21 14:09:21 +0300

    исправления по комм.препода

--- task1/Program.cs
+++ task1/Program.cs
@@ -3,7 +3,6 @@ int a = -9;
 int b = -3;
 int max = a;
 
-if (a > max) max = a;
 if (b > max) max = b;
 
 Console.Write("max = ");
--- task2/Program.cs
+++ task2/Program.cs
@@ -5,7 +5,6 @@ int b = 3;
 int c = 7;
 int max = a;
 
-if (a > max) max = a;
 if (b > max) max = b;
 if (c > max) max = c;
 
